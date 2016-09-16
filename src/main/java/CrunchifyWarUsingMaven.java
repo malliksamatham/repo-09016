48d950eca5558be9521d09f4f3665e13ebdf8711
root <root@ubuntu1.example.com>
2016-09-16 14:55:30 -0400

    testing

---
+++ src/main/java/CrunchifyWarUsingMaven.java
@@ -0,0 +1,13 @@
+/***additional commit
+$$$$$ 
+/**
+ * @author Crunchify.com
+ *
+ */
+ 
+public class CrunchifyWarUsingMaven {
+	public static void main(String[] args) {
+		System.out
+				.println("Test.. Test by Crunchify.. \nThis is a simple tutorial on how to create .war file using Maven Plugin..");
+	}
+}
